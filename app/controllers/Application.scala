6cf53a4cbc48143e712904a86dbf38cf43378e4b
trumanwu <chunan.woo@gmail.com>
2015-04-15 17:01:37 +0800

    add markdown support with chenkelmann/actuarius. demonstrate a simple but powerful example on index page.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,21 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
 
+import scala.io.Source
 import utils._
+
+import eu.henkelmann.actuarius.ActuariusTransformer
 
 object Application extends Controller {
 
 	def index = Action {
-		Ok("whipper-base is ready.")
+		val transformer = new ActuariusTransformer()
+		val mainMDFile = Play.getFile("public/markdowns/main.md")
+		val mainMDFileContent = Source.fromFile(mainMDFile).getLines().mkString("\n")
+		val mkParseOutput = transformer(mainMDFileContent)
+		Ok(views.html.md(mkParseOutput))
 	}
 
 }
